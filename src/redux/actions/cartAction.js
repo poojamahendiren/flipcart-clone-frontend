95a888dd3579aed97607f89e5d096b1d9fb6dd9b
pooja <poojamahendiren99@gmail.com>
2023-02-14 18:23:21 +0530

    initial commit

---
+++ src/redux/actions/cartAction.js
@@ -0,0 +1,27 @@
+import * as actionTypes from '../constants/cartConstants';
+import axios from 'axios';
+
+const URL = 'http://localhost:8000'
+
+export const addToCart = (id, quantity) => async (dispatch) => {
+    try { 
+        const { data } = await axios.get(`${URL}/product/${id}`);
+        console.log(data);
+
+        dispatch({ type: actionTypes.ADD_TO_CART, payload: { ...data, quantity } });
+        
+
+    } catch (error) {
+        dispatch({ type: actionTypes.ADD_TO_CART_ERROR, payload: error.message });
+        console.log('Error while calling cart API');
+    }
+};
+
+export const removeFromCart = (id) => (dispatch) => {
+    
+    dispatch({
+        type: actionTypes.REMOVE_FROM_CART,
+        payload: id
+    })
+
+};
